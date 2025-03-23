283e755764c4d5bee0d26c9a1b6707022624324a
Azeem <b122310496@student.utem.edu.my>
2025-03-23 22:13:39 +0800

    edit readme

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ async function main() {
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
     try {
+
+        console.time("Connection time");//start the timer
+
         await client.connect();
         console.log("Connected to MongoDB");
+
+        console.timeEnd("Connection time");//end the timer
 
         const database = client.db("testDB");
         const collection = database.collection("users");
